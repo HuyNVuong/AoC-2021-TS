8a75b17b63ce5e8f197cad2134536f222fdf9992
HuyNVuong <vuongnguyenhuy@yahoo.com>
2021-12-06 21:26:17 -0600

    day06

--- src/day06.ts
+++ src/day06.ts
@@ -2,6 +2,11 @@ import { INPUT } from "./day06.input";
 
 const fishes = INPUT.split(',').map(Number);
 
+// 3,4,3,1,2
+// [0,1,1,2,1,0,0,0,0]
+// [1,1,2,1,0,0,0,0,0]
+// [1,2,1,0,0,0,1,0,1]
+// .....
 const stimulateFish = (fishes: number[], days: number): number => {
   const spawns = Array(9).fill(0);
   for (const fish of fishes) {
