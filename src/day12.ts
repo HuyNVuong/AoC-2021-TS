25546b6e694c54a926a2ea9983e28013c3015776
HuyNVuong <vuongnguyenhuy@yahoo.com>
2021-12-12 15:45:04 -0600

    day12

---
+++ src/day12.ts
@@ -0,0 +1,79 @@
+import { INPUT } from "./inputs/day12.input";
+
+const graphs = INPUT.split('\n').reduce((edgeList, line) => {
+  const [src, dest] = line.split('-');
+  if (!edgeList.has(src)) edgeList.set(src, []);
+  if (!edgeList.has(dest)) edgeList.set(dest, []);
+  edgeList.get(src)!.push(dest);
+  edgeList.get(dest)!.push(src);
+
+  return edgeList;
+}, new Map<string, string[]>());
+
+const isLowerCase = (str: string) => str.toLowerCase() === str;
+
+interface Node {
+  node: string,
+  seen: Set<string>,
+}
+
+const part01 = (graphs: Map<string, string[]>) => {
+  const queue = [{node: 'start', seen: new Set(['start'])}];
+  let pathsCount = 0;
+  while(queue.length > 0) {
+    const {node, seen} = queue.shift()!;
+    if (node === 'end') {
+      pathsCount++;
+      continue;
+    }
+    for (const neighbor of graphs.get(node)!) {
+      if (!seen.has(neighbor)) {
+        const newSeen = new Set([...seen]);
+        if (isLowerCase(neighbor)) newSeen.add(neighbor);
+        queue.push({node: neighbor, seen: newSeen});
+      }    
+    }  
+  }
+
+  return pathsCount;
+};
+
+const part02 = (graphs: Map<string, string[]>) => {
+  const queue = [{
+    node: 'start', 
+    seen: new Set(['start']), 
+    hasSpecialSmallCave: false,
+  }];
+  let pathsCount = 0;
+  while(queue.length > 0) {
+    const {node, seen, hasSpecialSmallCave} = queue.shift()!;
+    if (node === 'end') {
+      pathsCount++;
+      continue;
+    }
+    for (const neighbor of graphs.get(node)!) {
+      if (!seen.has(neighbor)) {
+        queue.push({
+          node: neighbor, 
+          seen: isLowerCase(neighbor) 
+            ? new Set([...seen, neighbor])
+            : new Set([...seen]), 
+          hasSpecialSmallCave
+        });
+      } else {
+        if (!hasSpecialSmallCave && isLowerCase(neighbor) && neighbor !== 'start') {
+          queue.push({
+            node: neighbor, 
+            seen: new Set([...seen, neighbor]), 
+            hasSpecialSmallCave: true
+          });
+        }
+      }
+    }  
+  }
+
+  return pathsCount;
+};
+
+console.log(part01(graphs));
+console.log(part02(graphs));
